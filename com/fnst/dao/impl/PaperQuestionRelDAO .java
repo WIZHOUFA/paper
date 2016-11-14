15e771a236bfc970a7f0ad1c7575aaacb3fff860
WIZHOUFA <wizhou123@163.com>
2016-11-14 11:38:42 +0800

    Create PaperQuestionRelDAO .java

---
+++ com/fnst/dao/impl/PaperQuestionRelDAO .java
@@ -0,0 +1,10 @@
+package com.fnst.paper.dao.impl;
+
+import org.springframework.stereotype.Repository;
+
+import com.fnst.paper.pojo.PaperQuestionRel;
+
+@Repository("paperQuestionRelDAO")
+public class PaperQuestionRelDAO extends BaseDAOImpl<PaperQuestionRel>{
+
+}
